fb4815f689de187245cada06081dcb0eaa5ae13c
Iryna Yehorova <interest4me@gmail.com>
2024-01-31 01:24:44 +0200

    feat: add implement up command

--- src/commands/up.js
+++ src/commands/up.js
@@ -1,4 +1,20 @@
 // Go upper from current directory (when you are in the root folder this operation shouldn't change working directory)
 // up
-const up = () => {}
-export {up};
+import { homedir } from "node:os";
+import { parse } from "node:path";
+
+const up = () => {
+  const userHomeDir = homedir();
+  console.log("userHomeDir", userHomeDir);
+
+  const currentDirrectory = process.cwd();
+  console.log("currentDirrectory", currentDirrectory);
+  console.log("userHomeDir === currentDirrectory", userHomeDir === currentDirrectory);
+  const newCurrentDir = (userHomeDir === currentDirrectory)?
+                        userHomeDir :
+                        process.chdir('..');
+  console.log("Now you are in ", process.cwd());
+
+
+}
+export { up };
