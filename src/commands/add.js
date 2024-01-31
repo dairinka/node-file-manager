300ba4f394f70af3163dba962549affe78c33111
Iryna Yehorova <interest4me@gmail.com>
2024-01-31 15:36:50 +0200

    feat: implement add command

--- src/commands/add.js
+++ src/commands/add.js
@@ -1,4 +1,25 @@
 //Create empty file in current working directory:
 // add new_file_name
-const add = (newFileName) => {}
-export {add};
+
+import { access, writeFile } from 'node:fs/promises';
+import { messageErrorOperationFailed } from '../utility/messages/messageErrorOperationFailed.js';
+
+const add = async (newFileName) => {
+  try {
+    await access(newFileName);
+    throw Error('exist');
+  } catch (err) {
+    if (err.message === "exist") {
+      console.log(`\x1b[31mOperation failed: \x1b[0mFile ${newFileName} already exist `);
+      return;
+    }
+  }
+  try {
+    await writeFile(newFileName, '');
+    console.log('\x1b[32mFile was successfully added\x1b[0m')
+  } catch {
+    messageErrorOperationFailed();
+  }
+  
+}
+export { add };
