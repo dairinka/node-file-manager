777bd3f6793dd11ebf786f418b0ba61a5d445d4c
Iryna Yehorova <interest4me@gmail.com>
2024-01-31 09:05:32 +0200

    fix: remove extra code

--- src/commands/cd.js
+++ src/commands/cd.js
@@ -1,32 +1,21 @@
 // Go to dedicated folder from current directory (path_to_directory can be relative or absolute)
 // cd path_to_directory
 import { parse } from 'path';
-import { homedir } from 'os';
-
 
 const cd = (path) => {
-  if (isDirectoryAboveHomeDir(path)) return process.chdir(homedir());
   if (!isCorrectRootDir(path)) return process.chdir(path + '/');
-  // TODO case cd ../
   process.chdir(path);
 }
 
-function getPatheDetailes(path) {
+function getPathDetailes(path) {
   const pathDetailes = parse(path);
   const root = pathDetailes.root?.toLowerCase();
   const pathDirectories = pathDetailes.dir;
   return { root, pathDirectories };
 }
 
-function isDirectoryAboveHomeDir(path) {
-  const { root, pathDirectories } = getPatheDetailes(path);
-  const homedirPath = parse(homedir()).dir;
-
-  if (root.startsWith('c') && !pathDirectories.startsWith(homedirPath)) return true;
-}
-
 function isCorrectRootDir(path) {
-  const { root, pathDirectories } = getPatheDetailes(path);
+  const { root, pathDirectories } = getPathDetailes(path);
   if (root.search(/^[c-j]:$/i) != -1 && pathDirectories.length == 2) {
     return false;
   }
--- src/commands/up.js
+++ src/commands/up.js
@@ -1,20 +1,9 @@
 // Go upper from current directory (when you are in the root folder this operation shouldn't change working directory)
 // up
-import { homedir } from "node:os";
-import { parse } from "node:path";
 
 const up = () => {
-  const userHomeDir = homedir();
-  console.log("userHomeDir", userHomeDir);
 
-  const currentDirrectory = process.cwd();
-  console.log("currentDirrectory", currentDirrectory);
-  console.log("userHomeDir === currentDirrectory", userHomeDir === currentDirrectory);
-  const newCurrentDir = (userHomeDir === currentDirrectory)?
-                        userHomeDir :
-                        process.chdir('..');
+  process.chdir('..');
   console.log("Now you are in ", process.cwd());
-
-
 }
 export { up };
