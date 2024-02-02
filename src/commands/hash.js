36b6ff1e33d9ef4f4e25083852a4dd056c95be25
Iryna Yehorova <interest4me@gmail.com>
2024-02-02 15:11:59 +0200

    feat: implement hash command

--- src/commands/hash.js
+++ src/commands/hash.js
@@ -1,6 +1,13 @@
 // Calculate hash for file and print it into console
 // hash path_to_file
-const hash = (pathToFile) => {
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { stdout } from 'process';
 
+const hash = (pathToFile) => {
+  const ch = createHash('sha256');
+  const input = createReadStream(pathToFile);
+  input.pipe(ch).setEncoding('hex').pipe(stdout);
+  stdout.on('unpipe', () => console.log(''));
 }
 export {hash};
